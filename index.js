54e1afee699cc7dee004684cc19a7edf7c70e5de
Lmcmaaser <52054766+Lmcmaaser@users.noreply.github.com>
2019-11-22 17:08:29 -0700

    updated js

--- index.js
+++ index.js
@@ -3,10 +3,7 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
         document.getElementById("navbar").style.padding = "5px 10px";
-        //document.getElementById("logo").style.fontSize = "25px";
     } else {
         document.getElementById("navbar").style.padding = "20px 10px";
-        //document.getElementById("logo").style.fontSize = "35px";
-    }
-    
+    } 
 }
